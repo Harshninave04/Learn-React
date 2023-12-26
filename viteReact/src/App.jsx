fe4ea2b97152b9b7d41800afcb4cfeee4dca194c
Harsh Ninave <harshninave58@gmail.com>
2023-12-26 23:45:23 +0530

    Vite bundler for creating REACT apps

---
+++ viteReact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Hello React!!</h1>
+      <h3>Started learning React from the youtube instructor `HITESH CHAUDHARY`.</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ viteReact/src/about.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <h3>
+        My name is Harsh Ninave, Pursuing Btech Degree from JD COLLEGE OF ENGINEERNG AND MANAGEMENT.
+        I am in 3rd year with the overall CGPA 9.4
+      </h3>
+    </>
+  );
+}
+
+export default About;
