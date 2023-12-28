5d82968fb5e47ca0cacb8cde72379fc20d4e79fd
Harsh Ninave <harshninave58@gmail.com>
2023-12-28 17:08:52 +0530

    Custom React working understanding

--- viteReact/src/App.jsx
+++ viteReact/src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
+  const username = "Harsh Ninave";
+
   return (
     <>
       <h1>Hello React!!</h1>
+      <h3>I am {username},</h3>
       <h3>Started learning React from the youtube instructor `HITESH CHAUDHARY`.</h3>
     </>
   );
--- viteReact/src/main.jsx
+++ viteReact/src/main.jsx
@@ -1,13 +1,25 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import About from "./about.jsx";
 
+// Can write the function/components inside the file itself, instead of creating a another file but this is not considered as a good practice. Always for fast refresh move your component to a separate file
+function Product() {
+  return (
+    <>
+      <h1>Product Section</h1>
+      <h3>Diversity of Product helps you pick the right one for you !!</h3>
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <App />
     <About />
-  </React.StrictMode>
+    <Product />
+  </>
 );
