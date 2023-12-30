b1adcd3b9d62df280f60ead39523083284f77023
Harsh Ninave <harshninave58@gmail.com>
2023-12-30 20:14:25 +0530

    Background Color Changer

--- bg-Changer/src/App.jsx
+++ bg-Changer/src/App.jsx
@@ -1,10 +1,99 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [color, setColor] = useState("black");
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 font-bold">
+        <div className="flex flex-wrap gap-3 justify-center bg-white px-6 py-3 rounded-full cursor-pointer">
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("red")}
+            style={{ backgroundColor: "red" }}>
+            RED
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("green")}
+            style={{ backgroundColor: "green" }}>
+            GREEN
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("blue")}
+            style={{ backgroundColor: "blue" }}>
+            BLUE
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("orange")}
+            style={{ backgroundColor: "orange" }}>
+            ORANGE
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("violet")}
+            style={{ backgroundColor: "violet" }}>
+            VOILET
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-black outline-black "
+            onClick={() => setColor("yellow")}
+            style={{ backgroundColor: "yellow" }}>
+            YELLOW
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-black outline-black "
+            onClick={() => setColor("pink")}
+            style={{ backgroundColor: "pink" }}>
+            PINK
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("red")}
+            style={{ backgroundColor: "red" }}>
+            RED
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("green")}
+            style={{ backgroundColor: "green" }}>
+            GREEN
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("blue")}
+            style={{ backgroundColor: "blue" }}>
+            BLUE
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("orange")}
+            style={{ backgroundColor: "orange" }}>
+            ORANGE
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-white outline-black "
+            onClick={() => setColor("violet")}
+            style={{ backgroundColor: "violet" }}>
+            VOILET
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-black outline-black "
+            onClick={() => setColor("yellow")}
+            style={{ backgroundColor: "yellow" }}>
+            YELLOW
+          </button>
+          <button
+            className="outline-none px-4 py-2 rounded-full text-black outline-black "
+            onClick={() => setColor("pink")}
+            style={{ backgroundColor: "pink" }}>
+            PINK
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
