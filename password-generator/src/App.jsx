84bb58fe6af8a17f87cec7bfe4128130dd6e807a
Harsh Ninave <harshninave58@gmail.com>
2024-01-02 00:16:43 +0530

    useEffect hook used and can generate password by length

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -13,19 +13,19 @@ function App() {
     if (number) string += "0123456789";
     if (character) string += "~`!@#$%^&*()_+{}|[]";
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * string.length + 1);
-      pass = string.charAt(char);
+      pass += string.charAt(char);
     }
     setPassword(pass);
   }, [length, number, character, setPassword]);
 
-  // passwordGenerator();
+  useEffect(() => passwordGenerator(), [length, number, character, passwordGenerator]);
 
   return (
     <>
-      <div className="w-full mx-auto my-8 px-4 py-3 max-w-md bg-gray-800 text-orange-500 shadow-md rounded-lg">
-        <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className="w-[100%] h-[200px] mx-auto my-8 px-4 py-6 max-w-md bg-gray-800 text-orange-500 shadow-md rounded-lg">
+        <h1 className="text-white text-center my-5 text-[28px]">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
@@ -34,13 +34,15 @@ function App() {
             placeholder="Password"
             readOnly
           />
-          <button className="outline-none bg-blue-700 text-white py-1 px-3 shrink-0">COPY</button>
+          <button className="outline-none bg-blue-700 text-white py-1 px-3 shrink-0 rounded-none">
+            COPY
+          </button>
         </div>
         <div className="flex text-sm gap-x-2">
           <div className="flex items-center gap-x-1">
             <input
               type="range"
-              min={6}
+              min={8}
               max={40}
               value={length}
               className="cursor-pointer"
