9bcb8d4e216a7935b9cdda0cd0010b71afbfeb4a
Harsh Ninave <harshninave58@gmail.com>
2024-01-24 22:41:18 +0530

    contact form edited

--- reactRouterDom/src/Components/Contact/Contact.jsx
+++ reactRouterDom/src/Components/Contact/Contact.jsx
@@ -36,7 +36,7 @@ export default function Contact() {
                   />
                 </svg>
                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  Acme Inc, Street, State, Postal Code
+                  Wathoda Layout, Nagpur-440008
                 </div>
               </div>
 
@@ -56,7 +56,7 @@ export default function Contact() {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <div className="ml-4 text-md tracking-wide font-semibold w-40">+44 1234567890</div>
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">+91 9359436297</div>
               </div>
 
               <div className="flex items-center mt-2 text-gray-600">
@@ -75,7 +75,9 @@ export default function Contact() {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <div className="ml-4 text-md tracking-wide font-semibold w-40">info@acme.org</div>
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  harshninave58@gmail.com
+                </div>
               </div>
             </div>
 
@@ -116,6 +118,18 @@ export default function Contact() {
                   id="tel"
                   placeholder="Telephone Number"
                   className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
+              </div>
+
+              <div className="flex flex-col mt-2">
+                <label htmlFor="message" className="hidden">
+                  Message
+                </label>
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Type your message here"
+                  className="w-full mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none resize-none"
                 />
               </div>
 
