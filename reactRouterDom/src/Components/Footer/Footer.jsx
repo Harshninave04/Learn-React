254165bf410798a1b19341a769464b3b35c815fc
Harsh Ninave <harshninave58@gmail.com>
2024-01-24 22:15:55 +0530

    Adjusted logo and styling

--- reactRouterDom/src/Components/Footer/Footer.jsx
+++ reactRouterDom/src/Components/Footer/Footer.jsx
@@ -7,11 +7,7 @@ export default function Footer() {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <Link to="/" className="flex items-center">
-              <img
-                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                className="mr-3 h-16"
-                alt="Logo"
-              />
+              <img src="./public/logo.png" className="mr-3 h-16" alt="Logo" />
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -35,7 +31,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/Harshninave04"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer">
