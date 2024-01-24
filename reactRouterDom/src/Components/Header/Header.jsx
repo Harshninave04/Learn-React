68ec05f25f88b881c521730f9bd5956c14e3d30f
Harsh Ninave <harshninave58@gmail.com>
2024-01-24 22:13:34 +0530

    H website logo

--- reactRouterDom/src/Components/Header/Header.jsx
+++ reactRouterDom/src/Components/Header/Header.jsx
@@ -6,11 +6,7 @@ export default function Header() {
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
-            <img
-              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-              className="mr-3 h-12"
-              alt="Logo"
-            />
+            <img src="./public/logo.png" className="mr-3 h-12" alt="Logo" />
           </Link>
           <div className="flex items-center lg:order-2">
             <Link
@@ -48,6 +44,17 @@ export default function Header() {
                     } border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }>
                   About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/Github"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b ${
+                      isActive ? "text-orange-700" : "text-gray-700"
+                    } border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }>
+                  Github
                 </NavLink>
               </li>
               <li>
