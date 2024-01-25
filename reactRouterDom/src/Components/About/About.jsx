e81d48fddd6323c0403f0247e53912d7afe78ec7
Harsh Ninave <harshninave58@gmail.com>
2024-01-25 23:03:30 +0530

    Intro about me

--- reactRouterDom/src/Components/About/About.jsx
+++ reactRouterDom/src/Components/About/About.jsx
@@ -6,23 +6,20 @@ export default function About() {
       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div className="md:5/12 lg:w-5/12">
-            <img
-              src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
-              alt="image"
-            />
+            <img src="./public/vector-home.jpg" alt="image" />
           </div>
           <div className="md:7/12 lg:w-6/12">
             <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-              React development is carried out by passionate developers
+              Reacting with Purpose, Building with Passion
             </h2>
             <p className="mt-6 text-gray-600">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-              accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-              aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+              I am <bold className="font-bold text-lg">Harsh Ninave,</bold> a 19-year-old Frontend
+              Developer in India, skilled in crafting impactful websites. I want to pivot my career
+              toward DevOps and backend development to broaden my professional expertise. I believe
+              in mixing creativity with technical skills to make awesome online experiences
             </p>
             <p className="mt-4 text-gray-600">
-              Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-              Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+              React is not about making things easier; It is about making things predictable.
             </p>
           </div>
         </div>
